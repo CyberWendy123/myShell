43a1aa7a399f58317fd4ef07a112fa02611f980f
CyberWendy123 <xiaowenx@alumni.wfu.edu>
2019-09-06 13:23:08 -0400

    another one bites the dust

--- main.c
+++ main.c
@@ -1,30 +1,38 @@
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <stdbool.h> //the boolean library 
+#include <ctype.h> //isspace function 
 #include <string.h> 
 
-void trimLine(char* line)
-{ 	//remove leading/trailing whitespace 
+char* trimLine(char* str) //remove leading/trailing whitespace 
+{ 	//tried to find code without memory leaks 
+	char *end; 
+	while(isspace((unsigned char)*str)) str++; //trim leading 
+	end = str + strlen(str) - 1; 
+	while(end> str && isspace((unsigned char)* end)) end--; 
+	*(end + 1) = '\0'; 
+	return (str); 
 }
 
 int main(int argc, char *argv[])
 {
 	int byte_size = 250; 
-	char *command = malloc(byte_size * sizeof(char)); 
-	char *quit = "q"; 
+	char *enteredValue = malloc(byte_size * sizeof(char)); 
+	char *command; //cleaned up version 
+	char *quit = "quit()"; 
 
-	do{
+	do{ // a do while loop 
 		printf("$: ");
-		scanf("%s", command); 
+		scanf("%s", enteredValue); 
 		
-		command[strlen(command) - 1] = 0; //remove \n at the end
-		trimLine(command); //remove leading/trailing whitespace 
+		enteredValue[strlen(enteredValue) - 1] = '\0'; //remove \n at the end
+		command = trimLine(enteredValue); //remove leading/trailing whitespace 
 
 		//if q, quit program 
 		if(strcmp(command, quit))
 			break; 
 	} while(1); 
 
-	free(command); 
+	free(enteredValue); 
 	return 0; 
 }
